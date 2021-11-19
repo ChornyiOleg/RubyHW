fff781e00e4786d223b8154225c7de859083c811
ChornyiOleg <olegchornyi82@gmail.com>
2021-11-19 10:44:09 +0200

    even odd indexes

---
+++ HW1/tasks.rb
@@ -0,0 +1,21 @@
+
+ p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.'
+
+
+ p "intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]"
+ 
+ p "intArr.select.each_with_index { |_, i| i.even? }"
+
+ p "intArr.select.each_with_index { |_, i| i.odd? }"
+
+
+
+p intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]
+ 
+p intArr.select.each_with_index { |_, i| i.even? }
+
+p intArr.select.each_with_index { |_, i| i.odd? }
+
+
+p '-----------------------------------------------------'
+
