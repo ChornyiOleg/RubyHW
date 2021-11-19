7674c33040a5b239419d32aa756150dfc4cb126e
ChornyiOleg <olegchornyi82@gmail.com>
2021-11-19 20:29:50 +0200

    odd even inexes

--- HW1/tasks.rb
+++ HW1/tasks.rb
@@ -1,12 +1,12 @@
 
- p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.'
+p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с четными индексами, а затем - с нечетными.'
 
 
- p "intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]"
+p "intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]"
  
- p "intArr.select.each_with_index { |_, i| i.even? }"
+p "intArr.select.each_with_index { |_, i| i.even? }"
 
- p "intArr.select.each_with_index { |_, i| i.odd? }"
+p "intArr.select.each_with_index { |_, i| i.odd? }"
 
 
 
@@ -19,3 +19,23 @@ p intArr.select.each_with_index { |_, i| i.odd? }
 
 p '-----------------------------------------------------'
 
+
+p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными.'
+
+
+p "intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]"
+
+p "intArr.select.each_with_index { |_, i| i.odd? }"
+
+p "intArr.select.each_with_index { |_, i| i.even? }"
+
+
+p intArr = [7, 2, 9, 3, 55, 89, 45, 44, 367, 18, 23, 110]
+ 
+p intArr.select.each_with_index { |_, i| i.odd? }
+
+p intArr.select.each_with_index { |_, i| i.even? }
+
+p '-----------------------------------------------------'
+
+ 
