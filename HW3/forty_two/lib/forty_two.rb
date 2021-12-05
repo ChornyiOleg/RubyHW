a8a10b5cd5a19507f37e85ec6134baa4fac3948a
ChornyiOleg <olegchornyi82@gmail.com>
2021-12-06 00:23:13 +0200

    42 is working

--- HW3/forty_two/lib/forty_two.rb
+++ HW3/forty_two/lib/forty_two.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+class Integer < Numeric; end
+
+# Forty Two for extending Integer
+
+# rubocop:disable Style/Documentation
+
 class Integer
   def forty_two?
     self == 42
